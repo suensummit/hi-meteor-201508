6a954073d09027251ca25263be31a93365441a3a
Summit Suen <summit.suen@gmail.com>
2015-08-12 02:03:18 +0800

    sort by submit time

--- main.js
+++ main.js
@@ -1,33 +1,16 @@
 // Global
-console.log("Both sides");
 Message = new Mongo.Collection("message");
-
-// Sample Messages
-sampleMessages = [
-  {text : "gaaa", from : "db"},
-  {text : "heee", from : "db"},
-  {text : "yaaa", from : "db"}
-]
 
 // Client
 if (Meteor.isClient) {
-  console.log("Client side");
   // body: for loop
   Template.body.helpers({
     Msgs: function() {
-      return(Message.find())
+      return(Message.find({},{sort:{createdAt:-1}}))
     }
-    // Msgs: sampleMessages
-    // Msgs: [
-    //   {n : 1, text : "gaaa"},
-    //   {n : 2, text : "heee"},
-    //   {n : 3, text : "yaaa"}
-    // ]
   })
   Template.body.events({
     "change #inputMsg" : function(e, t){
-      // console.log("hello log");
-      // console.log($(e.target).val());
       msg = $(e.target).val();
       $(e.target).val("");
       msgData = {
@@ -41,10 +24,4 @@ if (Meteor.isClient) {
 
 // Server
 if (Meteor.isServer) {
-  console.log("Server side");
-  if (Message.find().count() === 0) {
-    for (i in sampleMessages) {
-      Message.insert(sampleMessages[i])
-    }
-  }
 }
