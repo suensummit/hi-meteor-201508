e72bea056c28a33633ed756e470995c63fa8ce7a
Summit Suen <summit.suen@gmail.com>
2015-08-14 16:27:24 +0800

    update template guestBook

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ Message = new Mongo.Collection("message");
 // Client
 if (Meteor.isClient) {
   // body: for loop
-  Template.body.helpers({
+  Template.guestBook.helpers({
     Msgs: function() {
       return(Message.find({},{sort:{createdAt:-1}}))
     }
   })
-  Template.body.events({
+  Template.guestBook.events({
     "change #inputMsg" : function(e, t){
       msg = $(e.target).val();
       // usr = $("#inputUsr").val();
       // if (!usr){
       //   usr = "Anonymous"
       // }
-      $("form > input").val("");
+      $("input").val("");
       msgData = {
         text : msg,
         createdAt : new Date,
